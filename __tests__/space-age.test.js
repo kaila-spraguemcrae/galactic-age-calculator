fd4bc78486ffde7657a662c86bf8049ddba0d3fa
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-13 13:32:32 -0800

    refactor lifeExectancy

--- __tests__/space-age.test.js
+++ __tests__/space-age.test.js
@@ -7,6 +7,8 @@ describe( 'SpaceAge', () => {
   beforeEach(() => {
     spaceAge = new SpaceAge("Kaila", 28, 80);
     spaceAge.convertAge();
+    spaceAge.lifeExpectancy();
+    console.log(spaceAge.lifeArr);
   });
 
   test('Should create and instance of SpaceAge', () => {
@@ -16,34 +18,34 @@ describe( 'SpaceAge', () => {
   });
 
   test('Should calculate age into Mercury years', () => {
-    expect(spaceAge.newArr[0]).toEqual(117);
+    expect(spaceAge.ageArr[0]).toEqual(117);
   });
 
   test('Should calculate age into Venus years', () => {
-    expect(spaceAge.newArr[1]).toEqual(45);
+    expect(spaceAge.ageArr[1]).toEqual(45);
   });
 
   test('should calculate age into Mars years', () => {
-    expect(spaceAge.newArr[2]).toEqual(15);
+    expect(spaceAge.ageArr[2]).toEqual(15);
   }); 
 
   test('Should calculate age into Jupiter years', () => {
-    expect(spaceAge.newArr[3]).toEqual(2);
+    expect(spaceAge.ageArr[3]).toEqual(2);
   });
 
   test('Should calculate remaining years on Mercury according to life expectancy', () => {
-    expect(spaceAge.mercuryLife()).toEqual(216);
+    expect(spaceAge.lifeArr[0]).toEqual(216);
   });
 
-  test('Should calculate how many years user has left to live on Venus', () => {
-    expect(spaceAge.venusLife()).toEqual(84);
-  });
+  // test('Should calculate how many years user has left to live on Venus', () => {
+  //   expect(spaceAge.venusLife()).toEqual(84);
+  // });
 
-  test('Should calculate how many years user has left to live on Mars', () => {
-      expect(spaceAge.marsLife()).toEqual(28);
-  });
+  // test('Should calculate how many years user has left to live on Mars', () => {
+  //     expect(spaceAge.marsLife()).toEqual(28);
+  // });
 
-  test('Should calculate how many years user has left to live on Jupiter', () => {
-    expect(spaceAge.jupiterLife()).toEqual(5);
-  });
+  // test('Should calculate how many years user has left to live on Jupiter', () => {
+  //   expect(spaceAge.jupiterLife()).toEqual(5);
+  // });
 });
--- src/js/space-age.js
+++ src/js/space-age.js
@@ -3,33 +3,44 @@ export default class SpaceAge {
     this.name = name;
     this.age = age;
     this.life = life;
-    this.newArr = []
+    this.ageArr = [];
+    this.lifeArr = [];
   }
 
   convertAge(){
     let arr = [0.24, 0.62, 1.88, 11.86] 
     for(let i=0; i < arr.length; i++) {
-      this.newArr.push(Math.round(this.age / arr[i]));
+      this.ageArr.push(Math.round(this.age / arr[i]));
     }
   }
 
-  mercuryLife() {
-    let result = Math.round(this.life / 0.24);
-    return result - this.newArr[0]; 
+  lifeExpectancy(){
+    let arr = [0.24, 0.62, 1.88, 11.86] 
+    arr.forEach((num1, i) => {
+      let result = Math.round(this.life / num1);
+      if (result > this.ageArr[i]) {
+        this.lifeArr.push(result - this.ageArr[i]);
+      } 
+    });
   }
 
-  venusLife() {
-    let result = Math.round(this.life / 0.62);
-    return result - this.newArr[1];
-  }
+  // mercuryLife() {
+  //   let result = Math.round(this.life / 0.24);
+  //   return result - this.newArr[0]; 
+  // }
 
-  marsLife() {
-    let result = Math.round(this.life / 1.88);
-    return result - this.newArr[2];
-  }
+  // venusLife() {
+  //   let result = Math.round(this.life / 0.62);
+  //   return result - this.newArr[1];
+  // }
 
-  jupiterLife() {
-    let result = Math.round(this.life / 11.86);
-    return result - this.newArr[3];
-  }
+  // marsLife() {
+  //   let result = Math.round(this.life / 1.88);
+  //   return result - this.newArr[2];
+  // }
+
+  // jupiterLife() {
+  //   let result = Math.round(this.life / 11.86);
+  //   return result - this.newArr[3];
+  // }
 };
