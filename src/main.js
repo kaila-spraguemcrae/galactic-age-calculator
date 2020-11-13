a687ddda42f4635ace717a78c5862b256ca3e493
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-13 15:16:20 -0800

    add basic html, css and grabs input from user

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-import SpaceAge from './js/space-age.js'; 
+// import SpaceAge from './js/space-age.js'; 
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
+  event.preventDefault();
 
+  $(".galactic-age").show();
+
+  const inputtedName = $("input#name").val();
+  const inputtedAge = parseInt($("input#age").val());
+  const inputtedLife = parseInt($("input#life-expectancy").val());
+
+  
 });
